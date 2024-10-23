05e46b2744e6865593fa41bb04c6c8d868a2c21d
Aguero0109 <67283117+Aguero0109@users.noreply.github.com>
2024-10-23 20:57:03 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,9 @@
+const nameGirl = '❤️❤️❤️';
+const giftUrl = 'http://nodemy.vn';
+const eventName = 'Em Yêu Của Anh';
+const titleCard = 'Em Yêu Của Anh';
+const contentCard = 'Gửi em yêu của anh. Mong cho em luôn vui vẻ, mạnh khoẻ, hạnh phúc, nhiều may mắn và những điều tốt đẹp nhất sẽ đến với em yêu của anh ❤️';
+
+const giftImage = '1234.jpg';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
